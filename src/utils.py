af84298aeaae190fa709aba244cbdf8896fa17b7
Achille <achille.gellens@gmail.com>
2022-12-19 15:14:03 +0100

    feat: update utils.py
    Added a function to plot a TensorFlow training history

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
+# ----- CONSTANTS -----
 data_dir = "../res"
 
 x = np.load(os.path.join(data_dir, "x_train.npy"))
@@ -23,3 +25,31 @@ class_loss_weights = [
 initial_class_biases = [
     np.log(class_counts[k] / (total_n_samples - class_counts[k])) for k in range(n_labels)
 ]
+# --------------------
+
+def plot_training_history(history):
+    best_epoch = np.argmax(history['val_accuracy'])
+    plt.figure(figsize=(17, 4))
+    plt.plot(history['loss'], label='Training loss', alpha=.8, color='#ff7f0e')
+    plt.plot(history['val_loss'], label='Validation loss', alpha=.9, color='#5a9aa5')
+    plt.axvline(x=best_epoch, label='Best epoch', alpha=.3, ls='--', color='#5a9aa5')
+    plt.title('Categorical Crossentropy')
+    plt.legend()
+    plt.grid(alpha=.3)
+    plt.show()
+
+    plt.figure(figsize=(17, 4))
+    plt.plot(history['accuracy'], label='Training accuracy', alpha=.8, color='#ff7f0e')
+    plt.plot(history['val_accuracy'], label='Validation accuracy', alpha=.9, color='#5a9aa5')
+    plt.axvline(x=best_epoch, label='Best epoch', alpha=.3, ls='--', color='#5a9aa5')
+    plt.title('Accuracy')
+    plt.legend()
+    plt.grid(alpha=.3)
+    plt.show()
+
+    plt.figure(figsize=(18, 3))
+    plt.plot(history['lr'], label='Learning Rate', alpha=.8, color='#ff7f0e')
+    plt.axvline(x=best_epoch, label='Best epoch', alpha=.3, ls='--', color='#5a9aa5')
+    plt.legend()
+    plt.grid(alpha=.3)
+    plt.show()
